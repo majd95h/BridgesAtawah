fb52bc9feb0f514529664fff61dd9d178697da99
Majd Hsien <majdhsien1@gmail.com>
2026-09-09 13:39:42 +0300

    fixes

--- stock_package_automation/__manifest__.py
+++ stock_package_automation/__manifest__.py
@@ -9,11 +9,7 @@
     ],
     'data': [
         'security/ir.model.access.csv',
-
         'views/stock_move_destination_wizard_views.xml',
-
-        'views/stock_move_line_views.xml',
-        'views/stock_move_views.xml',
     ],
     'installable': True,
     'auto_install': False,
